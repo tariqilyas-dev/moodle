fedb73472b89c2ac6402c75eea66017fb7d3e3e1
tariqilyas-dev <tariqilyas301@gmail.com>
2021-05-28 20:30:15 +0530

    add file reader plugin

---
+++ local/file_reader/README.md
@@ -0,0 +1,8 @@
+# Local message
+
+This is a fiile reader plugin for moodle. It will upload, serach and delete the course and activity.
+
+## Functionality
+- UPload course and activity
+- Search the course and activity
+- Delete course and activity
---
+++ local/file_reader/get_file.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle Course Rollover Plugin
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package     local_message
+ * @author      Kristian
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(__DIR__ . '/../../config.php');
+
+global $DB;
+
+$PAGE->set_url(new moodle_url('/local/file_reader/get_file.php'));
+$PAGE->set_context(\context_system::instance());
+$PAGE->set_title('File Reader');
+
+$pagedata = $DB->get_records('page');
+
+echo $OUTPUT->header();
+
+foreach ($pagedata as $data) {
+}
+
+// echo $data->id; 
+// echo $data->course; exit;
+
+$templatecontext = (object)[
+	'messages' => array_values($messages),
+    'deleteurl' => new moodle_url('/local/file_reader/delete_file.php'),
+];
+
+echo $OUTPUT->render_from_template('local_file_reader/get_file', $templatecontext);
+
+echo $OUTPUT->footer();
